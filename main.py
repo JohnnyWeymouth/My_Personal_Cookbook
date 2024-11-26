528755276c62d68da3f851cc5c10d87e68fcaa76
JohnnyWeymouth <johnnycweymouth@gmail.com>
2024-11-26 15:11:16 -0800

    change flask to run as production

--- main.py
+++ main.py
@@ -412,4 +412,4 @@ def change_password():
 
 # listen on port 8080
 if __name__ == '__main__':
-    app.run(port=8080, debug=False) # TODO: Students PLEASE remove debug=True when you deploy this for production!!!!!
+    app.run(host='0.0.0.0', port=8080, debug=False)
