2be591650c58327a1a269517c5923800807082ec
JohnnyWeymouth <johnnycweymouth@gmail.com>
2024-12-04 15:39:20 -0700

    hotfix on recipe model

--- Models/Recipe.py
+++ Models/Recipe.py
@@ -1,29 +1,7 @@
-import base64
-from datetime import date
-from pydantic import BaseModel
-
-class Recipe(BaseModel):
-    recipe_id: int
-    recipe_name: str
-    date_created: date
-    recipe_image: 
-    recipe_description: str
-    instructions: str
-    tags: list[str]
-    user_id: int
-    def __init__(self, **data):
-        if 'recipe_image' in data and isinstance(data['recipe_image'], bytes):
-            data['recipe_image'] = base64.b64encode(data['recipe_image']).decode('utf-8')
-        else:
-            data['recipe_image'] = None
-        super().__init__(**data)
-
-
 from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 from datetime import date
 import base64
-
 
 class Recipe(BaseModel):
     recipe_id: int
