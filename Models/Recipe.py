8cce298cf2a2a20292ed0f8a15f2641889f59b9b
JohnnyWeymouth <johnnycweymouth@gmail.com>
2024-12-04 16:00:21 -0700

    attempt 5

--- Models/Recipe.py
+++ Models/Recipe.py
@@ -8,15 +8,8 @@ class Recipe(BaseModel):
     recipe_id: int
     recipe_name: str = Field(..., max_length=255)
     date_created: date
-    recipe_image: Optional[str] # "Base64 encoded recipe image."
+    recipe_image: bytes
     recipe_description: str = Field(..., max_length=3000)
     instructions: str = Field(..., max_length=3000)
     tags: list[str]
     user_id: int
-
-    @field_validator("recipe_image")
-    def encode_recipe_image(cls, value):
-        """Encodes the image in Base64 if it's provided as bytes."""
-        if isinstance(value, bytes):
-            return base64.b64encode(value).decode('utf-8')
-        return value
